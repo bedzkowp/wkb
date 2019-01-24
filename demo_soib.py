f2b13fcec78827ab64994b510b152c33832e7f8f
BartoszKosciug <34551054+BartoszKosciug@users.noreply.github.com>
2019-01-24 19:15:25 +0100

    catch ValueError

--- demo_soib.py
+++ demo_soib.py
@@ -19,141 +19,62 @@ class Display(BoxLayout):
 
 class LoginScreen(GridLayout):
 
-   
-    
     def n(self,r,a,g,delta,n1):
         if r >=0 and r <= a:
           return n1 * math.sqrt(1 - 2 * delta * ((r / a)**g))
         else:
           return n1 * math.sqrt(1 - 2 * delta)
 
-    def plotN(self, instance):
-        if(len(self.m.text) == 0 or len(self.p.text) == 0 or len(self.g.text) == 0 or len(self.n1.text)==0 or len(self.NA.text)==0 or len(self.clad_d.text)==0 or len(self.core_d.text)==0 or len(self.l.text)==0):
-            layout = GridLayout(cols=1)
-            layout.add_widget(Label(text="Proszę podać wszystkie parametry!"))
-            btnClose = Button(text="Zamknij")
-            layout.add_widget(btnClose)
-
-            popup = Popup(title="UWAGA!", content=layout,size_hint=(None,None), size=(300,200),auto_dismiss=False)
-            btnClose.bind(on_press=popup.dismiss)
-            popup.open()
-        else:
-            m = int(self.m.text)
-            p = int(self.p.text)
-            g = int(self.g.text)
-            n1 = float(self.n1.text)
-            NA = float(self.NA.text)
-            core_d = float(self.core_d.text)/10**6
-            clad_d = float(self.clad_d.text)/10**6
-            l = float(self.l.text)/10**9
-            delta = (NA**2)/(2*(n1**2))
-            clad_r = clad_d/2
-            a = core_d/2
-            r_vec = linspace(0,clad_r)
-            n_vec = []
-            
-            for r in r_vec:
-                n_vec.append(self.n(r,a, g, delta, n1))
-                
-            plt.plot(r_vec,n_vec)
-            plt.xlabel("odległość od środka światłowodu - r [m]")
-            plt.ylabel("wartość współczynnika załamania - n")
-            plt.title("Wykres współczynnika załamania od odległości od środka światłowodu")
-            plt.grid(True)
-            plt.show()
-    
-    def plotB(self, instance):
-        if(len(self.m.text) == 0 or len(self.p.text) == 0 or len(self.g.text) == 0 or len(self.n1.text)==0 or len(self.NA.text)==0 or len(self.clad_d.text)==0 or len(self.core_d.text)==0):
-            layout = GridLayout(cols=1)
-            layout.add_widget(Label(text="Proszę podać wszystkie parametry!"))
-            btnClose = Button(text="Zamknij")
-            layout.add_widget(btnClose)
-            
-            popup = Popup(title="UWAGA!", content=layout,size_hint=(None,None), size=(300,200),auto_dismiss=False)
-            btnClose.bind(on_press=popup.dismiss)
-            popup.open()
-        else:
-            #popup = Popup(title="Progress Bar", content=self.pb)
-            #popup.open()
-            m = int(self.m.text)
-            p = int(self.p.text)
-            g = int(self.g.text)
-            n1 = float(self.n1.text)
-            NA = float(self.NA.text)
-            core_d = float(self.core_d.text)/10**6
-            clad_d = float(self.clad_d.text)/10**6
-            
-            delta = (NA**2)/(2*(n1**2))
-            clad_r = clad_d/2
-            a = core_d/2
-            l_vec = linspace(600e-9,1600e-9, 20)
-            l_vec2 = linspace(600,1600,20)
-            b_vec = []
-            #self.trigger()
-            
-            for l in l_vec:
-                ko = (2 * math.pi) / l 
-                b_vec.append(beta(NA,g, n1,l, core_d, clad_d,m,p)['b']/10**6)
-                #self.update_bar_trigger()
-                #Clock.schedule_once(self.update_bar,1)
-                
-            plt.plot(l_vec2,b_vec)
-            plt.xlabel("długość fali - [nm]")
-            plt.ylabel("B [rad/um]")
-            plt.title("Wykres B/ko w zależności od długości fali")
-            plt.grid(True)
-            plt.show()
-	
-	
-    def isFilled(self,instance):
+    def popUp(self, _text, _width=300):
+        layout = GridLayout(cols=1)
+        layout.add_widget(Label(text=_text))
+        btnClose = Button(text="Zamknij")
+        layout.add_widget(btnClose)
         
-        if(len(self.m.text) == 0 or len(self.p.text) == 0 or len(self.g.text) == 0 or len(self.n1.text)==0 or len(self.NA.text)==0 or len(self.clad_d.text)==0 or len(self.core_d.text)==0 or len(self.l.text)==0):
-            layout = GridLayout(cols=1)
-            layout.add_widget(Label(text="Proszę podać wszystkie parametry!"))
-            btnClose = Button(text="Zamknij")
-            layout.add_widget(btnClose)
+        popup = Popup(title="UWAGA!", content=layout,size_hint=(None,None), size=(_width,200),auto_dismiss=False)
+        btnClose.bind(on_press=popup.dismiss)
+        popup.open()
 
-            popup = Popup(title="UWAGA!", content=layout,size_hint=(None,None), size=(300,200),auto_dismiss=False)
-            btnClose.bind(on_press=popup.dismiss)
-            popup.open()
+    def isFilled(self,instance):
+        is_filled_good = True
+        if(len(self.m.text) == 0 or len(self.p.text) == 0 or len(self.g.text) == 0 or len(self.n1.text)==0 or len(self.NA.text)==0 or len(self.clad_d.text)==0 or len(self.core_d.text)==0 or len(self.l.text)==0):
+
+            self.popUp("Prosze podać wszystkie parametry!")
+            
         else:
             m = int(self.m.text)
             p = int(self.p.text)
-            g = int(self.g.text)
+            g = float(self.g.text)
             n1 = float(self.n1.text)
             NA = float(self.NA.text)
             core_d = float(self.core_d.text)/10**6
             clad_d = float(self.clad_d.text)/10**6
             l = float(self.l.text)/10**9
-            try:
-                B = beta(NA, g, n1, l, core_d, clad_d, m, p)
-                self.B.text = str(round(B['b']/10**6,3))				
-            except ValueError:
-                layout = GridLayout(cols=1)
-                layout.add_widget(Label(text="Proszę podać odpowiednie parametry!"))
-                btnClose = Button(text="Zamknij")
-                layout.add_widget(btnClose)
-                popup = Popup(title="UWAGA!", content=layout,size_hint=(None,None), size=(300,200),auto_dismiss=False)
-                btnClose.bind(on_press=popup.dismiss)
-                popup.open()
+
+            if not(m >= 0):
+                self.popUp("Zła wartość m!")
+                is_filled_good = False
+            if not(p >= 1):
+                self.popUp("Zła wartość p!")
+                is_filled_good = False 
+            if not(NA >= 0):
+                self.popUp("Proszę podać aperture numeryczną > 0",400)
+                is_filled_good = False 
+            if (core_d > clad_d):
+                self.popUp("Średnica rdzenia nie może być większa niż średnica płaszcza!",500)
+                is_filled_good = False
+
+            if is_filled_good == True:
+                try:
+                    B = beta(NA, g, n1, l, core_d, clad_d, m, p)
+                    self.B.text = str(round(B['b']/10**6,3))				
+                except ValueError:
+                    self.popUp("Złe parametry!")
+                    self.B.text = "Proszę podać odpowiednie parametry!"
 			
-           
 
-    def update_bar(self,dt):
-
-            self.pb.value = self.pb.value + dt
-            #self.update_bar_trigger()
-            if self.pb.value > 20:
-                Clock.unschedule(self.update_bar)
-
-    def trigger(self):
-        self.pb.value = 0
-        #self.update_bar_trigger() 
-    
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        #self.update_bar_trigger = Clock.create_trigger(self.update_bar,-1)
-        self.pb = ProgressBar()
         
         self.cols = 2
         
@@ -193,12 +114,6 @@ class LoginScreen(GridLayout):
         self.B = TextInput(readonly=True)
         self.add_widget(self.B)
 
-        self.add_widget(Button(text="Wykres współczynnika zał.", on_press= self.plotN))
-        #self.B = TextInput(readonly=True)
-        #self.add_widget(self.B)
-        self.add_widget(Button(text="Wykres stałej propagacji B", on_press= self.plotB))
-        
-            
 class Screen_One(Screen):
     def __init__(self, **kwargs):
         super(Screen_One, self).__init__(**kwargs)
